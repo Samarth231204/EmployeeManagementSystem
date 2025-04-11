ae7ca388dcc58524e53e7dcea16a9dba9a9d336b
Samarth <samarthgoss@gmail.com>
2025-04-12 00:32:15 +0530

    Employee Dashboard

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # EmployeeManagementSystem
-<<<<<<< HEAD
-This is a reach based website for task assignment and tracking, leveraging Local Storage for persistence.
-=======
-This is a reach based website for task assignment and tracking, leveraging Local Storage for persistence.
->>>>>>> 04a94cd (updated readme)
+This is a reach based website for task assignment and tracking, leveraging Local Storage for 
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('email is', email)
+    console.log('password is', password) 
+    setEmail('')
+    setPassword('')
+  }
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
         <div className='border-2 rounded-xl border-emerald-600 p-20'>
-            <form className='flex flex-col items-center justify-center'>
-                <input required className='border-2 border-emerald-600 font-medium rounded-full text-xl text-white outline-none bg-transparent py-2 px-6 placeholder:text-gray-400' type="email" placeholder='Enter your email' />
-                <input required className='border-2 border-emerald-600 font-medium rounded-full mt-3 text-xl text-white outline-none bg-transparent py-2 px-6 placeholder:text-gray-400' type="passsword" placeholder='Enter your password' />
+            <form
+            onSubmit = {(e)=>{
+              submitHandler(e)
+            }}
+             className='flex flex-col items-center justify-center'>
+                <input
+                value={email}
+                onChange={(e)=>{
+                  setEmail(e.target.value)
+                }}
+                required 
+                className='border-2 border-emerald-600 font-medium rounded-full text-xl text-white outline-none bg-transparent py-2 px-6 placeholder:text-gray-400' type="email" placeholder='Enter your email' />
+                <input
+                value={password}
+                onChange={(e)=>{
+                  setPassword(e.target.value)
+                }}
+                required 
+                className='border-2 border-emerald-600 font-medium rounded-full mt-3 text-xl text-white outline-none bg-transparent py-2 px-6 placeholder:text-gray-400' type="password" placeholder='Enter your password' />
                 <button className='border-none oultine-none hover:bg-emerald-700 bg-emerald-600 font-semibold rounded-full mt-7 text-lg text-white py-2 px-8 w-full placeholder:text-white'>Log in</button>
             </form>
         </div> 
