54f8a6d757770fb0c89cdfdfc3180bed94db2f25
Samarth <samarthgoss@gmail.com>
2025-04-13 04:50:18 +0530

    employee and admin page/ separating user and admin data

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage } from './utils/LocalStorage'
-import { setLocalStorage } from './utils/LocalStorage'
+import { AuthContext } from './context/AuthProvider'
 
 
 const App = () => {
 
-  useEffect(() => {
-    setLocalStorage()
-    getLocalStorage()
-  },)
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+ 
+  const handleLogin =(email,password)=>{
+    if(email == 'admin@me.com' && password == '123'){
+      setUser('admin')
+
+    }
+    else if(authData && authData.employees.find((e)=>email==e.email && e.password == password)){
+      setUser('user')
+
+    }
+    else{
+      alert('Invalid credentials')
+    }
+  }
+
 
   return (
     <>
-      <Login/>
-      {/* <EmployeeDashboard/> */}
-      {/* <AdminDashboard/> */}
+      {!user ? <Login handleLogin={handleLogin} />: null}
+      {user=="admin"? <AdminDashboard/>:<EmployeeDashboard/>}
     </>
   )
   }
