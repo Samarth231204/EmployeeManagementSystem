b8014dea3e1a3ef2ea8675a767cbec32b85901ab
Samarth <samarthgoss@gmail.com>
2025-04-13 02:40:02 +0530

    local Storage Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { getLocalStorage } from './utils/LocalStorage'
+import { setLocalStorage } from './utils/LocalStorage'
+
 
 const App = () => {
+
+  useEffect(() => {
+    setLocalStorage()
+    getLocalStorage()
+  },)
+
   return (
     <>
-      {/* <Login/> */}
+      <Login/>
       {/* <EmployeeDashboard/> */}
-      <AdminDashboard/>
+      {/* <AdminDashboard/> */}
     </>
   )
-}
+  }
 
 export default App
